4c8622dd7d6002f5c90a69e3407ebebacba53328
Marilena Medhat <176769293+MarilenaMedhat@users.noreply.github.com>
2024-11-21 01:35:14 +0200

    feat: add operate function

--- index.js
+++ index.js
@@ -1,67 +1,80 @@
-function add(a, b) {
-  const variableOne = a;
-  const variableTwo = b;
-  const operator = "+";
-  operation(variableOne, variableTwo, operator);
-  return a + b;
-}
-
-function substract(a, b) {
-  const variableOne = a;
-  const variableTwo = b;
-  const operator = "-";
-  operation(variableOne, variableTwo, operator);
-  return a - b;
-}
-
-function multiply(a, b) {
-  const variableOne = a;
-  const variableTwo = b;
-  const operator = "*";
-  operation(variableOne, variableTwo, operator);
-  return a * b;
-}
-
-function divide(a, b) {
-  const variableOne = a;
-  const variableTwo = b;
-  const operator = "/";
-  operation(variableOne, variableTwo, operator);
-  return a / b;
-}
-
-function operation(variableOne, variableTwo, operator) {
-  return console.log(variableOne, variableTwo, operator);
-}
-
 const display = document.querySelector(".display");
-const buttons = document.querySelectorAll(".btn");
+const buttons = document.querySelectorAll(".btn:not(.operator)");
+const operators = document.querySelectorAll(".operator");
 
 let currentInput = "";
+let variableOne = "";
+let variableTwo = "";
+let operator = "";
 
+function operate(variableOne, variableTwo, operator) {
+  switch (operator) {
+    case "+":
+      return variableOne + variableTwo;
+    case "-":
+      return variableOne - variableTwo;
+    case "*":
+      return variableOne * variableTwo;
+    case "/":
+      return variableOne / variableTwo;
+    default:
+      return variableOne;
+  }
+}
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const value = button.textContent;
-    // if(value==="+"){
-    //     add();
-    // }if(value==="-"){
-    //     substract();
-    // }if(value==="*"){
-    //     multiply();
-    // if(value==="/"){
-    //     divide();
-    currentInput += value;
-    display.textContent = currentInput;
+    if (!operator) {
+      if (variableOne.length >= 7) {
+        return;
+      }
+      variableOne += value;
+    } else {
+      if (!variableTwo) {
+        display.textContent = "";
+      }
+      if (variableTwo.length >= 7) {
+        return;
+      }
+      variableTwo += value;
+    }
+    console.log({ variableOne, variableTwo });
+    display.textContent += value;
+  });
+});
+
+operators.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+    if ("+*/-".includes(value) && !operator) {
+      operator = value;
+      //   display.textContent = value;
+      return;
+    }
+    if (value === "=") {
+      const result = operate(
+        Number(variableOne),
+        Number(variableTwo),
+        operator
+      );
+      display.textContent = result;
+      variableOne = result;
+      variableTwo = "";
+      operator = "";
+      return;
+    }
   });
 });
 
 const clear = document.querySelector("#clear");
 clear.addEventListener("click", () => {
   display.textContent = "";
-  currentInput = "";
+  variableOne = "";
+  variableTwo = "";
+  operator = "";
 });
-const sign = document.querySelector("#sign");
-sign.addEventListener("click", () => {
-  currentInput = "-" + currentInput;
-  display.textContent = currentInput;
-});
+// const sign = document.querySelector("#sign");
+// sign.addEventListener("click", () => {
+//   currentInput = "-" + currentInput;
+//   display.textContent = currentInput;
+// });
